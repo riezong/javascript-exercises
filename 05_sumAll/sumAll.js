ee30e2e31771fa175017239fbc58bad0af8f2ae0
Andrew <71823699+riezong@users.noreply.github.com>
2024-11-23 10:34:56 +1100

    Complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,46 @@
-const sumAll = function() {
+const sumAll = function(fNumber, lNumber) {
+
+    if (fNumber < 0 || lNumber < 0) {
+        return "ERROR";
+    }
+
+    if (fNumber % 1 > 0 || lNumber % 1 > 0) {
+        return "ERROR";
+    }
+
+    // if (isNaN(fNumber) || isNaN(lNumber)) {
+    //     return "ERROR";
+    // }
+
+    if (typeof fNumber !== 'number' || typeof lNumber !== 'number') {
+        return "ERROR";
+    }
+    
+
+    // Make first number smaller than last number
+    let startNumber;
+    let endNumber;
+
+    if (fNumber > lNumber) {
+        startNumber = lNumber;
+        endNumber = fNumber;
+    } else {
+        startNumber = fNumber;
+        endNumber = lNumber;
+    }
+
+
+    // Create array with all numbers filled in
+    const sumOf = [];
+
+    for (let i = startNumber; i <= endNumber; i++) {
+        sumOf.push(i);
+    }
+
+    // reduce function to sum
+    let sum = sumOf.reduce((a, b) => a + b, 0);
+
+    return sum;
 
 };
 
