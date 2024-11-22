df8b0e5475a21680a88ad0fc02ff404b5221fd1c
Andrew <71823699+riezong@users.noreply.github.com>
2024-11-22 16:46:34 +1100

    Complete exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let newString = "";
+    if (num >= 0) {
+        for (i = 0; i < num; i++) {
+            newString = newString.concat(string);
+        };
+        return newString;
+    } else {
+        return "ERROR";
+    }
+    
 };
 
 // Do not edit below this line
