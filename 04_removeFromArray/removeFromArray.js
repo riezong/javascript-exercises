c42543c74990c44fe625be82c690fec890bb7405
Andrew <71823699+riezong@users.noreply.github.com>
2024-11-23 09:59:43 +1100

    Complete exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(input, ...remove) {
+
+    const newArray = [];
+
+    for (const number of input) {
+        // for (let i = 0; i < remove.length; i++) {
+        //     if (number == remove[i]) {
+        //         continue;
+        //     } else {
+        //         newArray.push(number);
+        //     }
+        // }
+        if (!remove.includes(number)) {
+            newArray.push(number);
+          }
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
