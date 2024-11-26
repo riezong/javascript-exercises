2b23b0ee8c3685020f32fd20102780da006ca5f7
Andrew <71823699+riezong@users.noreply.github.com>
2024-11-26 15:19:16 +1100

    Complete exercise 08_calculator
    Need to revise .reduce method
    Need to revise factorial function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(...add) {
+	result = [...add].reduce((acc, val) => acc + val, 0);
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(...sub) {
+	result = [...sub].reduce((acc, val) => acc - val);
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(sum) {
+	result = sum.reduce((acc, val) => acc + val, 0);
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(multi) {
+  result = multi.reduce((acc, val) => acc * val);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+	result = Math.pow(num, pow);
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(fac) {
+  let f = [];
+	if (fac == 0 || fac == 1) {
+    return 1;
+  } else if (f[fac] > 0) {
+      return f[fac];
+  }
+  return f[fac] = factorial(fac-1) * fac;
 };
 
 // Do not edit below this line
