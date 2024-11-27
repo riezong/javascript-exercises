238c78a5d7b614f6143fb2e1126648c2678536dd
Andrew <71823699+riezong@users.noreply.github.com>
2024-11-27 13:27:01 +1100

    Complete exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,25 @@
-const palindromes = function () {
+const palindromes = function (input) {
+
+    // Remove punctuation
+    let punctuation = /[\.,?! ]/g;
+    text = input.replace(punctuation, "");
+    text = text.toLowerCase();
+
+    // Convert to array and reverse
+    let str = text.split("");
+    let reverseText = str.reverse().join("");
+
+    console.log(text);
+    console.log(reverseText);
+    
+    const compareValue = reverseText.localeCompare(text)
+    console.log(compareValue);
+    
+    if (compareValue == 0) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
