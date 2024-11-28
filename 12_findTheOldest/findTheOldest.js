23995de4f9036bbd33a5eba28a599b98d0828e4c
Andrew <71823699+riezong@users.noreply.github.com>
2024-11-28 17:28:14 +1100

    Complete exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    const age = [];
+    const date = new Date();
+    let currentYear = date.getFullYear();
+    let oldest = 0;
+  
+    for (const person of people) {
+        if (person.yearOfDeath > 0) {
+        age.push(person.yearOfDeath - person.yearOfBirth);
+        } else {
+        age.push(currentYear - person.yearOfBirth);
+        }
+    }
+
+    for (let i = 0; i < age.length; i++) {
+        if (age[i] > oldest) {
+        oldest = age[i];
+        }
+    }
+    
+    let index = age.indexOf(oldest);
+    
+    return people[index];
 
 };
 
